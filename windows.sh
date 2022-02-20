e39531bd0da921f38dc0fcfe02421fad2f9e58b0
stuin <stuin01@gmail.com>
2022-02-20 03:47:28 -0800

    Fixed windows build

--- windows.sh
+++ windows.sh
@@ -1,11 +1,10 @@
-files="src/Skyrmion/GridMaker.cpp src/Skyrmion/Node.cpp src/Skyrmion/UpdateList.cpp"
-res="res/*.o"
+files="src/main.cpp src/Skyrmion/GridMaker.cpp src/Skyrmion/Node.cpp src/Skyrmion/UpdateList.cpp  src/Skyrmion/LightMap.cpp"
 options="-pthread -O3 -std=c++17 -Wall -fpermissive -mwindows -static -DSFML_STATIC"
 libs="-lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lwinmm -lgdi32 -lopengl32 -lfreetype"
 
 output="ThePathBelow"
 
-cmd="x86_64-w64-mingw32-g++ $files $res -o $output.exe $options $libs"
+cmd="x86_64-w64-mingw32-g++ $files -o $output.exe $options $libs"
 
 echo $cmd
 $cmd
