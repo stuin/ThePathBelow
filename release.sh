f770a6401a92d943228a1fd4da6aa1219c2d87c3
stuin <stuin01@gmail.com>
2024-08-14 17:38:07 -0700

    Settings + release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 files="src/main.cpp src/Skyrmion/*.cpp"
-options="-pthread -O3 -std=c++17 -Wall -fpermissive -static -DSFML_STATIC"
-windows_libs="-mwindows -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lwinmm -lgdi32 -lopengl32 -lfreetype"
-linux_libs="-lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lX11"
+options="-pthread -O3 -std=c++17 -Wall -fpermissive"
+windows_libs="-static -DSFML_STATIC -mwindows -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lwinmm -lgdi32 -lopengl32 -lfreetype"
+linux_libs="-lsfml-graphics -lsfml-window -lsfml-system -lX11"
 
 output="ThePathBelow"
 
@@ -15,7 +15,8 @@ echo $linux_cmd
 $linux_cmd
 
 echo "Done, Creating zip"
+mv res/settings.json dev_settings.json
 cp res/default_settings.json res/settings.json
 zip -r "$output.zip" "$output.*" res/
-cp res/dev_settings.json res/settings.json
+mv dev_settings.json res/settings.json
 
